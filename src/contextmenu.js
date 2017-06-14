49dba6b284732e76b157f62c2d28ed39c4e3051e
Edouard Oger <eoger@fastmail.com>
2017-06-14 18:43:57 -0400

    Make sure the context menu never overflows
    Fixes #22

--- src/contextmenu.js
+++ src/contextmenu.js
@@ -1,6 +1,9 @@
-function ContextMenu(posX, posY, items) {
-  this.posX = posX;
-  this.posY = posY;
+// Make sure the menu doesn't stick the the sides of the sidebar
+const OVERFLOW_MENU_MARGIN = 6;
+
+function ContextMenu(clickPosX, clickPosY, items) {
+  this.clickPosX = clickPosX;
+  this.clickPosY = clickPosY;
   this.items = items;
   this.rootNode = null;
 }
@@ -9,8 +12,6 @@ ContextMenu.prototype = {
   show() {
     this.rootNode = document.createElement("ul");
     this.rootNode.classList = "contextmenu";
-    this.rootNode.style.left = `${this.posX}px`;
-    this.rootNode.style.top = `${this.posY}px`;
     const fragment = document.createDocumentFragment();
     for (let { label, onCommandFn } of this.items) {
       let item;
@@ -27,6 +28,23 @@ ContextMenu.prototype = {
     }
     this.rootNode.appendChild(fragment);
     document.body.appendChild(this.rootNode);
+    this.positionMenu();
+  },
+  positionMenu() {
+    const menuWidth = this.rootNode.offsetWidth + OVERFLOW_MENU_MARGIN;
+    const menuHeight = this.rootNode.offsetHeight + OVERFLOW_MENU_MARGIN;
+    const winWidth = window.innerWidth;
+    const winHeight = window.innerHeight;
+
+    const top = this.clickPosY + menuHeight > winHeight ?
+                (winHeight - menuHeight) :
+                this.clickPosY;
+    const left = this.clickPosX + menuWidth > winWidth ?
+                 (winWidth - menuWidth) :
+                 this.clickPosX;
+
+    this.rootNode.style.top = `${top}px`;
+    this.rootNode.style.left = `${left}px`;
   },
   hide() {
     if (this.rootNode) {
